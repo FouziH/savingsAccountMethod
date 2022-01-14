bed11c3aa5d2886da5dbf8c6dad955f295905c9a
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-14 07:42:46 -0600

    ...

--- Store.java
+++ Store.java
@@ -1,27 +1,28 @@
 public class Store {
 
-    //Instance fields 
+    // Instance fields
     String productType;
 
-
-    //Constructor method
+    // Constructor method
 
     public Store(String product) {
         productType = product;
 
     }
 
-    //advertise method
+    // advertise method
 
     public void advertise() {
+        System.out.println("Selling " + productType + "!");
+        System.out.println("Come spend some money!");
 
     }
 
-    //main method
-    public static void main (String[] args) {
+    // main method
+    public static void main(String[] args) {
+
+        Store lemonade = new Store("Lemonade");
 
     }
-
-
 
 }
